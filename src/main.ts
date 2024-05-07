18aaf5b5fd3efc4c33449825359982ace8ac643d
Santiago Lopez Amaya <santiagolopezamaya@hotmail.com>
2024-05-06 22:43:53 -0500

    :tada: comienzo

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+
+  const config = new DocumentBuilder()
+    .setTitle(configService.get<string>('SWAGGER_TITLE'))
+    .setDescription(configService.get<string>('SWAGGER_DESCRIPTION'))
+    .setVersion(configService.get<string>('SWAGGER_VERSION'))
+    .addTag(configService.get<string>('SWAGGER_TAG'))
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  const port = configService.get<number>('PORT') || 3000;
+  await app.listen(port);
 }
 bootstrap();
